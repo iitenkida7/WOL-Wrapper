d30ffb9dad80d7f82b69620e8a76827461c77a70
TakeshiIshii <takeshi.ishii@mfro.net>
2015-11-16 10:30:04 +0900

    文字化け対応

--- wol.php
+++ wol.php
@@ -2,12 +2,12 @@
 
 /*******
  *
- * wol(wake on lan) $B$N<B9T(BPHP$B$G$9!#(B
- * ether-wake $B%3%^%s%I$rMxMQ$7$F$$$^$9!#(B
- * ether-wake $B%3%^%s%I$O!"0lHLE*$K(B root $B8"8B$,I,MW$G$9!#(B
- * $B$3$N%W%m%0%i%`$G$O!"(Bsudo $B%3%^%s%I(B $B7PM3$G<B9T$7$F$$$^$9!#(B
- * sudo ether-wake  $B$G(B $B<B9T$G$-$k$h$&;vA0$K(Bsudo $B8"8B$rIUM?$7$F$/$@$5$$!#(B 
- * eth0 $B0J30$N%$%s%?!<%U%'!<%90J30$rMxMQ$9$k>l9g$O!"(B$srcEth $B$rJQ99$7$F$/$@$5$$!#(B
+ * wol(wake on lan) の実行PHPです。
+ * ether-wake コマンドを利用しています。
+ * ether-wake コマンドは、一般的に root 権限が必要です。
+ * このプログラムでは、sudo コマンド 経由で実行しています。
+ * sudo ether-wake  で 実行できるよう事前にsudo 権限を付与してください。 
+ * eth0 以外のインターフェース以外を利用する場合は、$srcEth を変更してください。
  *
  * *****/
 class WOL {
